9d41564d274a4304267d1542f7b5d90d953d56b5
David Duran <dadch1404@gmail.com>
2023-11-20 16:56:48 -0500

    Add remotePatterns for images

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 module.exports = {
   reactStrictMode: true,
   images: {
-    domains: ['res.cloudinary.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
   },
 }
