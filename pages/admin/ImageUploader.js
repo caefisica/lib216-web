c5e97187fb82cad5347da19098fd0126a21d23a9
David Duran <dadch1404@gmail.com>
2023-12-22 14:03:02 -0500

    Refactor ImageUploader component and sign API Fix: Next linting tests
    This commit refactors the ImageUploader component and the sign API. The changes include:
    - Reformatting the code to improve readability
    - Updating import statements to use single quotes
    - Adding semicolons to the end of statements
    - Adding error handling and logging in the sign API

--- pages/admin/ImageUploader.js
+++ pages/admin/ImageUploader.js
@@ -1,95 +1,95 @@
-import React, { useState, useCallback, useEffect } from "react"
-import { useDropzone } from "react-dropzone"
-import { Cloudinary } from "@cloudinary/url-gen"
-import { AdvancedImage, responsive, placeholder } from "@cloudinary/react"
-import Image from "next/image"
+import React, {useState, useCallback, useEffect} from 'react';
+import {useDropzone} from 'react-dropzone';
+import {Cloudinary} from '@cloudinary/url-gen';
+import {AdvancedImage, responsive, placeholder} from '@cloudinary/react';
+import Image from 'next/image';
 
-const cloudName = "dubu"
-const folderName = "thelibrary"
-const apiKey = process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY
+const cloudName = 'dubu';
+const folderName = 'thelibrary';
+const apiKey = process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY;
 
 const cld = new Cloudinary({
   cloud: {
     cloudName: cloudName,
   },
-})
+});
 
-export default function ImageUploader({ initialImage, onImageUpload }) {
-  const [uploadedFiles, setUploadedFiles] = useState([])
+export default function ImageUploader({initialImage, onImageUpload}) {
+  const [uploadedFiles, setUploadedFiles] = useState([]);
 
   const getSignature = async (folderName) => {
     try {
-      const response = await fetch("/api/sign", {
-        method: "POST",
+      const response = await fetch('/api/sign', {
+        method: 'POST',
         headers: {
-          "Content-Type": "application/json",
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ folder: folderName }),
-      })
+        body: JSON.stringify({folder: folderName}),
+      });
 
       if (!response.ok) {
-        throw new Error(`Network response was not ok: ${response.statusText}`)
+        throw new Error(`Network response was not ok: ${response.statusText}`);
       }
 
-      const data = await response.json()
-      return { signature: data.signature, timestamp: data.timestamp }
+      const data = await response.json();
+      return {signature: data.signature, timestamp: data.timestamp};
     } catch (error) {
-      console.error("Error fetching signature:", error)
-      throw error
+      console.error('Error fetching signature:', error);
+      throw error;
     }
-  }
+  };
 
   const onDrop = useCallback(
-    async (acceptedFiles) => {
-      const url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`
+      async (acceptedFiles) => {
+        const url = `https://api.cloudinary.com/v1_1/${cloudName}/upload`;
 
-      const { signature, timestamp } = await getSignature(folderName)
+        const {signature, timestamp} = await getSignature(folderName);
 
-      acceptedFiles.forEach(async (acceptedFile) => {
-        const formData = new FormData()
-        formData.append("file", acceptedFile)
-        formData.append("signature", signature)
-        formData.append("timestamp", timestamp)
-        formData.append("api_key", apiKey)
-        formData.append("folder", folderName)
+        acceptedFiles.forEach(async (acceptedFile) => {
+          const formData = new FormData();
+          formData.append('file', acceptedFile);
+          formData.append('signature', signature);
+          formData.append('timestamp', timestamp);
+          formData.append('api_key', apiKey);
+          formData.append('folder', folderName);
 
-        const response = await fetch(url, {
-          method: "post",
-          body: formData,
-        })
+          const response = await fetch(url, {
+            method: 'post',
+            body: formData,
+          });
 
-        if (!response.ok) {
-          const errorResponse = await response.json()
-          console.error("Fetch error:", response.statusText, errorResponse)
-          return
-        }
+          if (!response.ok) {
+            const errorResponse = await response.json();
+            console.error('Fetch error:', response.statusText, errorResponse);
+            return;
+          }
 
-        const data = await response.json()
-        setUploadedFiles((old) => [...old, data])
-        onImageUpload(data.secure_url)
-      })
-    },
-    [onImageUpload],
-  )
+          const data = await response.json();
+          setUploadedFiles((old) => [...old, data]);
+          onImageUpload(data.secure_url);
+        });
+      },
+      [onImageUpload],
+  );
 
   useEffect(() => {
     if (initialImage) {
-      setUploadedFiles([{ secure_url: initialImage }])
+      setUploadedFiles([{secure_url: initialImage}]);
     }
-  }, [initialImage])
+  }, [initialImage]);
 
-  const { getRootProps, getInputProps, isDragActive } = useDropzone({
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({
     onDrop,
-    accepts: "image/*",
+    accepts: 'image/*',
     multiple: false,
-  })
+  });
 
   return (
     <div>
       <div
         {...getRootProps()}
         className={`${
-          isDragActive ? "bg-blue-300" : "bg-gray-200"
+          isDragActive ? 'bg-blue-300' : 'bg-gray-200'
         } border p-4 rounded cursor-pointer`}
       >
         <input {...getInputProps()} />
@@ -101,7 +101,7 @@ export default function ImageUploader({ initialImage, onImageUpload }) {
           {uploadedFiles.map((file, i) => (
             <li key={i}>
               <AdvancedImage
-                style={{ maxWidth: "100%" }}
+                style={{maxWidth: '100%'}}
                 cldImg={cld.image(file.public_id)}
                 plugins={[responsive(), placeholder()]}
                 alt={`Uploaded Image ${i + 1}`}
@@ -123,5 +123,5 @@ export default function ImageUploader({ initialImage, onImageUpload }) {
         </div>
       )}
     </div>
-  )
+  );
 }
--- pages/api/sign.js
+++ pages/api/sign.js
@@ -1,28 +1,28 @@
-const cloudinary = require("../../config/cloudinaryConfig")()
-const { generateSignature } = require("../../utils/cloudinaryUtils")
+const cloudinary = require('../../config/cloudinaryConfig')();
+const {generateSignature} = require('../../utils/cloudinaryUtils');
 
 const handleRequest = async (req, res) => {
   try {
-    const timestamp = Math.round(new Date().getTime() / 1000)
-    const folderName = req.body.folder
-    const signature = generateSignature(cloudinary, timestamp, folderName)
+    const timestamp = Math.round(new Date().getTime() / 1000);
+    const folderName = req.body.folder;
+    const signature = generateSignature(cloudinary, timestamp, folderName);
 
-    res.status(200).json({ signature, timestamp })
+    res.status(200).json({signature, timestamp});
   } catch (error) {
-    console.error("Error Message:", error.message)
-    console.error("Error Stack:", error.stack)
+    console.error('Error Message:', error.message);
+    console.error('Error Stack:', error.stack);
     res
-      .status(500)
-      .json({ error: "Internal Server Error", details: error.message })
+        .status(500)
+        .json({error: 'Internal Server Error', details: error.message});
   }
-}
+};
 
 export const config = {
   // runtime: 'edge',
   unstable_allowDynamic: [
-    "**/node_modules/.pnpm/lodash@4.17.21/node_modules/lodash/*.js",
-    "**/node_modules/.pnpm/core-js@3.33.3/node_modules/core-js/internals/*.js",
+    '**/node_modules/.pnpm/lodash@4.17.21/node_modules/lodash/*.js',
+    '**/node_modules/.pnpm/core-js@3.33.3/node_modules/core-js/internals/*.js',
   ],
-}
+};
 
-export default handleRequest
+export default handleRequest;
