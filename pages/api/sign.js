ce65ac8a6576935f0bf7c00edbf4f3d51493af95
David Duran <dadch1404@gmail.com>
2023-12-22 14:10:53 -0500

    fix for cloudflare pages

--- pages/api/sign.js
+++ pages/api/sign.js
@@ -1,5 +1,5 @@
 const cloudinary = require('../../config/cloudinaryConfig')();
-const {generateSignature} = require('../../utils/cloudinaryUtils');
+const { generateSignature } = require('../../utils/cloudinaryUtils');
 
 const handleRequest = async (req, res) => {
   try {
@@ -7,18 +7,18 @@ const handleRequest = async (req, res) => {
     const folderName = req.body.folder;
     const signature = generateSignature(cloudinary, timestamp, folderName);
 
-    res.status(200).json({signature, timestamp});
+    res.status(200).json({ signature, timestamp });
   } catch (error) {
     console.error('Error Message:', error.message);
     console.error('Error Stack:', error.stack);
     res
-        .status(500)
-        .json({error: 'Internal Server Error', details: error.message});
+      .status(500)
+      .json({ error: 'Internal Server Error', details: error.message });
   }
 };
 
 export const config = {
-  // runtime: 'edge',
+  runtime: 'edge',
   unstable_allowDynamic: [
     '**/node_modules/.pnpm/lodash@4.17.21/node_modules/lodash/*.js',
     '**/node_modules/.pnpm/core-js@3.33.3/node_modules/core-js/internals/*.js',
