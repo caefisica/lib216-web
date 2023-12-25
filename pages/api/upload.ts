4f5b6c9fda696acfa9590d06ef25bd37961ac6a2
David Duran <dadch1404@gmail.com>
2023-12-25 00:47:55 -0500

    Add environment variable checks and error handling in upload API

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -8,7 +8,28 @@ export const config = {
   },
 };
 
+function checkEnvVariables(...variables: string[]): boolean {
+  return variables.every(
+    (variable) => typeof process.env[variable] === 'string',
+  );
+}
+
 export default async function POST(req: NextRequest) {
+  const areEnvVariablesDefined = checkEnvVariables(
+    'API_AUTH_TOKEN',
+    'CLOUDINARY_FOLDER_NAME',
+    'CLOUDINARY_API_KEY',
+    'NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME',
+  );
+
+  if (!areEnvVariablesDefined) {
+    console.error('Missing required environment variables');
+    return new NextResponse(
+      JSON.stringify({ error: 'Server configuration error' }),
+      { status: 500 },
+    );
+  }
+
   if (req.method !== 'POST') {
     return new NextResponse('Method Not Allowed', { status: 405 });
   }
@@ -23,17 +44,17 @@ export default async function POST(req: NextRequest) {
         'Content-Type': 'application/json',
         'x-api-token': process.env.API_AUTH_TOKEN,
       },
-      body: JSON.stringify({ folder: 'thelibrary' }),
+      body: JSON.stringify({ folder: process.env.CLOUDINARY_FOLDER_NAME }),
     });
 
     const { signature, timestamp } = await signResponse.json();
 
     const uploadFormData = new FormData();
-    uploadFormData.append('file', file); // Ensure compatibility with Cloudflare Workers
+    uploadFormData.append('file', file);
     uploadFormData.append('signature', signature);
     uploadFormData.append('timestamp', timestamp);
     uploadFormData.append('api_key', process.env.CLOUDINARY_API_KEY);
-    uploadFormData.append('folder', 'thelibrary');
+    uploadFormData.append('folder', process.env.CLOUDINARY_FOLDER_NAME);
 
     const cloudinaryResponse = await fetch(
       `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/upload`,
