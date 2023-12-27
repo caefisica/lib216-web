290cc144aa4a2f1677f23f6f82237f2769353e68
David Duran <dadch1404@gmail.com>
2023-12-26 20:26:51 -0500

    Add email verification functionality for sign-in

--- app/utils/auth.ts
+++ app/utils/auth.ts
@@ -1,5 +1,6 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth';
+import type { SendVerificationRequestParams } from 'next-auth/providers';
 import GithubProvider from 'next-auth/providers/github';
 import GoogleProvider from 'next-auth/providers/google';
 import prisma from './db';
@@ -18,5 +19,33 @@ export const {
       clientId: process.env.AUTH_GOOGLE_CLIENT_ID,
       clientSecret: process.env.AUTH_GOOGLE_CLIENT_SECRET,
     }) as any,
+    {
+      id: 'resend',
+      type: 'email',
+      async sendVerificationRequest({
+        identifier: email,
+        url,
+        token
+      }: SendVerificationRequestParams) {
+        const response = await fetch("https://api.resend.com/emails", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${process.env.AUTH_EMAIL_SERVER_PASSWORD}`,
+          },
+          body: JSON.stringify({
+            from: process.env.AUTH_EMAIL_FROM,
+            to: [email],
+            subject: "Your sign-in link",
+            html: `<p>Please use the following link to sign in:</p><p><a href="${url}">Sign in</a></p>`,
+          }),
+        });
+
+        if (!response.ok) {
+          const error = await response.text();
+          throw new Error(error);
+        }
+      },
+    },
   ],
 });
