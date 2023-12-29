7f878174b85248112ae6f72acf507d2097eda8b1
David Duran <dadch1404@gmail.com>
2023-12-29 03:16:03 -0500

    Add site configuration and title image
    This commit adds a new file `site.ts` containing site configuration details and a new image file `title.png` to the `public` directory.

---
+++ config/site.ts
@@ -0,0 +1,14 @@
+export const siteConfig = {
+  name: '@caefisica',
+  url: 'https://216.caefisica.com',
+  ogImage:
+    'https://uipxmbnthbibbjfgfvep.supabase.co/storage/v1/object/public/user%20image/cae.png',
+  description:
+    'Explora los libros disponibles en nuestra biblioteca en el salón 216 de la Facultad de Ciencias Físicas de la UNMSM.',
+  links: {
+    facebook: 'https://www.facebook.com/caefisica',
+    instagram: 'https://www.facebook.com/caefisica',
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
